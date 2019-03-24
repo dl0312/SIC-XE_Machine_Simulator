2ebf74ae757254a2130f030c9d2132bbd8c85873
undergraduate <cse20131582@cspro9.sogang.ac.kr>
2019-03-24 00:54:14 -0700

    feature: fill, just dump, dump with start, dump with start and end

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -56,7 +57,36 @@ void show(struct Record *hash_table[]);
 void opcodelist(struct Record *hash_table[]);
 int hash_function(int key);
 void edit(int traget_address, int data, void *addr);
+void fill(int start, int end, int data, void *addr);
 
+char* concat(int count, ...)
+{
+    va_list ap;
+    int i;
+
+    // Find required length to store merged string
+    int len = 1; // room for NULL
+    va_start(ap, count);
+    for(i=0 ; i<count ; i++)
+        len += strlen(va_arg(ap, char*));
+    va_end(ap);
+
+    // Allocate memory to concat strings
+    char *merged = calloc(sizeof(char),len);
+    int null_pos = 0;
+
+    // Actually concatenate strings
+    va_start(ap, count);
+    for(i=0 ; i<count ; i++)
+    {
+        char *s = va_arg(ap, char*);
+        strcpy(merged+null_pos, s);
+        null_pos += strlen(s);
+    }
+    va_end(ap);
+
+    return merged;
+}
 
 int main(void) {
 	// create hash table
@@ -70,14 +100,10 @@ int main(void) {
 	}
 
 	char input[MAX] = "";
-
-	char *cmd;
-	char *arg1 = NULL;
-	char *arg2 = NULL;
-	char *arg3;
+	char tmp_input[MAX] = "";
 
 	// init dump addresses
-	char addr[160] = { 0 };
+	unsigned char addr[16*65536] = { 0 };
 	char *ptr;
 
 	// dynamic allocate LinkedList of history
@@ -101,7 +127,7 @@ int main(void) {
 	while (!feof(ptr_file)) {
 		// fill out hash table with opcode.txt
 		fscanf(ptr_file, "%x %s %s", &inst_record.opcode, inst_record.mnemonic, inst_record.format);
-		// printf("%x %s %s\n", inst_record.opcode, inst_record.mnemonic, inst_record.format);
+		//printf("%x %s %s\n", inst_record.opcode, inst_record.mnemonic, inst_record.format);
 		insert(inst_record, hash_table);
 	}
 	
@@ -110,50 +136,60 @@ int main(void) {
 
 	while (1) {
 
+		// reset cmd, args
+		char *cmd;
+		char *arg1 = NULL;
+		char *arg2 = NULL;
+		char *arg3 = NULL;
+
 		// command input
 		printf("sicsim> ");
 		fgets(input, sizeof(input), stdin);
+		strcpy(tmp_input, input);	
+		tmp_input[strcspn(tmp_input, "\n")] = '\0';
 		int flag = 0;
 		int len = strlen(input);
 		cmd = input;
 		for (int i = 0; i < len; i++) {
 			if (flag == 0) {
-			// 첫 인자
+				// input cmd
 				if (input[i] > 96 && input[i] < 123) {
 					continue;
 				} else if (input[i] == ' ' || input[i] == '\t') {
-					printf("%d", i);
 					input[i] = '\0';
 					flag++;
+				} else if(input[i] == '\n'){
+					input[i] = '\0';
+					break;
 				}
 				else {
 					printf("wrong");
 				}
 			}
 			else if (flag == 1){
-			// 빈공간
+				// empty space
 				if (input[i] == ' ' || input[i] == '\t') {
 					continue;
 				}
 				else {
-					printf("%d", i);
 					arg1 = &input[i];
 					flag++;
 				}
 			}
 			else if (flag == 2){
-				// 두번째 인자 
+				// input arg1
 				if (input[i] == ' ' || input[i] == '\t') {
 					input[i] = '\0';
 					flag++;
-				}
-				else if (input[i] == ',') {
+				} else if (input[i] == '\n'){
+					input[i] = '\0';
+				} else if (input[i] == ',') {
 					input[i] = '\0';
 					flag += 2;
 				}
 			}
 			else if (flag == 3) {
-			// 빈공간 ','이전
+				// empty space before comma
 				if (input[i] == ' ' || input[i] == '\t') {
 					input[i] = '\0';
 				}
@@ -166,7 +202,7 @@ int main(void) {
 				}
 			}
 			else if (flag == 4) {
-				// 빈공간 ','이후
+				// empty space after comma
 				if (input[i] == ' ' || input[i] == '\t') {
 					input[i] = '\0';
 				}
@@ -176,20 +212,19 @@ int main(void) {
 				}
 			}
 			else if (flag == 5) {
-				// 세번째 인자 
-				if (input[i] == ' ' || input[i] == '\t') {
-				// 세번째 인자 
+				// input arg2
 				if (input[i] == ' ' || input[i] == '\t') {
 					input[i] = '\0';
 					flag++;
-				}
-				else if (input[i] == ',') {
+				} else if (input[i] == ',') {
 					input[i] = '\0';
 					flag += 2;
+				} else if(input[i] == '\n'){
+					input[i] = '\0';
 				}
 			}
 			else if (flag == 6) {
-				// 빈공간 ','이전
+				// empty space before comma
 				if (input[i] == ' ' || input[i] == '\t') {
 					input[i] = '\0';
 				}
@@ -202,7 +237,7 @@ int main(void) {
 				}
 			}
 			else if (flag == 7) {
-				// 빈공간 ','이후
+				// empty space after comma
 				if (input[i] == ' ' || input[i] == '\t') {
 					input[i] = '\0';
 				}
@@ -212,8 +247,8 @@ int main(void) {
 				}
 			}
 			else if (flag == 8) {
-				// 네번째 인자 
-				if (input[i] == ' ' || input[i] == '\t') {
+				// input arg3
+				if (input[i] == ' ' || input[i] == '\t' || input[i] == '\n') {
 					input[i] = '\0';
 					flag++;
 				}
@@ -225,63 +260,85 @@ int main(void) {
 		}
 		
 		//printf("%d\n", len);
-		printf("command: %s, argument1: %s, argument2: %s\n", cmd, arg1, arg2);
-		printf("%s %s strcmp: %d", cmd, "q\n", strcmp(cmd, "q\n"));		
+		printf("cmd: %s, arg1: %s, arg2: %s, arg3: %s\n", cmd, arg1, arg2, arg3);
+		//printf("%s %s strcmp: %d", cmd, "q\n", strcmp(cmd, "q\n"));		
 		
-		if (strcmp(cmd, "h\n") == 0 || strcmp(cmd, "help\n") == 0) {
+		if (strcmp(cmd, "h") == 0 || strcmp(cmd, "help") == 0) {
 			// h[elp]
 			display_help();
 		}
-		else if (strcmp(cmd, "d\n") == 0 || strcmp(cmd, "dir\n") == 0) {
+		else if (strcmp(cmd, "d") == 0 || strcmp(cmd, "dir") == 0) {
 			// d[ir]
 			display_dir();
 		}
-		else if (strcmp(cmd, "q\n") == 0 || strcmp(cmd, "quit\n") == 0) {
+		else if (strcmp(cmd, "q") == 0 || strcmp(cmd, "quit") == 0) {
 			// q[uit]
 			printf("quit shell\n");
 			break;
 		}
-		else if (strcmp(cmd, "hi\n") == 0 || strcmp(cmd, "history\n") == 0) {
+		else if (strcmp(cmd, "hi") == 0 || strcmp(cmd, "history") == 0) {
 			// hi[story]
-			createNode(L, cmd);
+			createNode(L, tmp_input);
 			printNodes(L);
 			continue;
 		}
-		else if (strcmp(cmd, "du\n") == 0 || strcmp(cmd, "dump\n") == 0) {
+		else if (strcmp(cmd, "du") == 0 || strcmp(cmd, "dump") == 0) {
 			// du[mp]
-			hexDump(&addr);
-		}
-		else if (strcmp(cmd, "e\n") == 0 || strcmp(cmd, "edit\n") == 0) {
+			int hex_arg1 = (int)strtol(arg1, NULL, 16);
+			int hex_arg2 = (int)strtol(arg2, NULL, 16);
+			if(arg1==NULL){
+				hexDump(&addr);
+			} else {
+				if(arg2==NULL){
+					hexDumpWithStart(hex_arg1,&addr);
+				}else{
+					if((hex_arg1 <= hex_arg2) && arg3==NULL){
+						hexDumpWithStartEnd(hex_arg1, hex_arg2, &addr);
+					}else{
+						continue;
+					}
+				}
+			}
+		} else if (strcmp(cmd, "e") == 0 || strcmp(cmd, "edit") == 0) {
 			// e[dit] address, data
-			edit((int)(arg1 - '0'), (int)((arg2[0] - '0') * 16 + (arg2[1] - '0')) , &addr);
-		}
-		else if (strcmp(cmd, "opcodelist\n") == 0) {
+			if(arg1!=NULL && arg2!=NULL){
+				edit(atoi(arg1), (int)strtol(arg2, NULL, 16), &addr);
+			}
+		} else if(strcmp(cmd, "f") == 0 || strcmp(cmd, "fill") == 0){
+			// f[ill] start, end, data
+			int hex_arg1 = (int)strtol(arg1, NULL, 16);
+			int hex_arg2 = (int)strtol(arg2, NULL, 16);
+			int hex_arg3 = (int)strtol(arg3, NULL, 16);
+			if((arg1!=NULL && arg2!=NULL) && arg3!=NULL){
+				fill(hex_arg1, hex_arg2, hex_arg3, &addr);
+			}
+		} else if (strcmp(cmd, "opcodelist") == 0) {
 			// opcodelist
 			opcodelist(hash_table);
+		} else {
+			// wrong command
+			printf("wrong command\n");
+			printf("if you want to know the commands h[elp]\n");
+			continue;
 		}
-			else {
-				// wrong command
-				printf("wrong command\n");
-				printf("if you want to know the commands h[elp]\n");
-				continue;
-			}
-			createNode(L, cmd);
-		}
+		createNode(L, tmp_input);
 	}
+
+	// quit machine
 	return 0;
 }
 
 void display_help(void) {
-	printf("\nh[elp]");
-	printf("\nd[ir]");
-	printf("\nq[uit]");
-	printf("\nhi[story]");
-	printf("\ndu[mp] [start, end]");
-	printf("\ne[dit] address, value");
-	printf("\nf[ill] start, end, value");
-	printf("\nreset");
-	printf("\nopcode mnemonic");
-	printf("\nopcodelist\n");
+	printf("h[elp]\n");
+	printf("d[ir]\n");
+	printf("q[uit]\n");
+	printf("hi[story]\n");
+	printf("du[mp] [start, end]\n");
+	printf("e[dit] address, value\n");
+	printf("f[ill] start, end, value\n");
+	printf("reset\n");
+	printf("opcode mnemonic\n");
+	printf("opcodelist\n");
 }
 
 void display_dir(void) {
@@ -305,7 +362,6 @@ void display_dir(void) {
 		}
 		closedir(d);
 	}
-	return(0);
 }
 
 void createNode(LinkedList *L, char cmd[]) {
@@ -320,13 +376,13 @@ void createNode(LinkedList *L, char cmd[]) {
 	}
 }
 
-// pirnt linked list history
+// print linked list history
 void printNodes(LinkedList *L) {
 	Node *p = L->head;
 	int cnt = 1;
 	while (p != NULL) {
 		printf("%d\t", cnt);
-		printf("%s", p->data);
+		printf("%s\n", p->data);
 		p = p->next;
 		cnt++;
 	}
@@ -347,11 +403,11 @@ void hexDump(void *addr)
 				printf("  %s\n", buff);
 
 			// Output the offset.
-			printf("  %04X ", i);
+			printf("  %05X ", i);
 		}
 
 		// Now the hex code for the specific character.
-		printf(" %02X", 0);
+		printf(" %02X", pc[i]);
 		//printf(" %02x", pc + sizeof(char));
 		//printf(" %x", pc + sizeof(char));
 		// And store a printable ASCII character for later.
@@ -378,10 +434,18 @@ void hexDump(void *addr)
 void hexDumpWithStart(int start, void *addr)
 {
 	int i;
-	unsigned char buff[17];
+	unsigned char buff[17] = {"."};
 	unsigned char *pc = (unsigned char*)addr;
+	for(i= start - start%16; i < start; i++){
+		if((i%16) == 0){
+			printf("  %05X ", i);
+		}
+		buff[i%16] = '.';
+		printf("   ");
+	
+	}
 	// Process every byte in the data.
-	for (i = 0; i < 160; i++) {
+	for (i = start; i < 160 + start; i++) {
 		// Multiple of 16 means new line (with line offset).
 
 		if ((i % 16) == 0) {
@@ -390,11 +454,11 @@ void hexDumpWithStart(int start, void *addr)
 				printf("  %s\n", buff);
 
 			// Output the offset.
-			printf("  %04X ", i);
+			printf("  %05X ", i);
 		}
 
 		// Now the hex code for the specific character.
-		printf(" %02X", pc + sizeof(char));
+		printf(" %02X", pc[i]);
 
 		// And store a printable ASCII character for later.
 		if ((pc[i] < 0x20) || (pc[i] > 0x7e)) {
@@ -406,12 +470,10 @@ void hexDumpWithStart(int start, void *addr)
 
 		buff[(i % 16) + 1] = '\0';
 	}
-
-	// Pad out last line if not exactly 16 characters.
-	while ((i % 16) != 0) {
+	for(i = start%16; i < 16; i++){
+		buff[i%16] = '.';
 		printf("   ");
-		i++;
-	}
+	}	
 
 	// And print the final ASCII bit.
 	printf("  %s\n", buff);
@@ -420,10 +482,18 @@ void hexDumpWithStart(int start, void *addr)
 void hexDumpWithStartEnd(int start, int end, void *addr)
 {
 	int i;
-	unsigned char buff[17];
+	unsigned char buff[17] = {"."};
 	unsigned char *pc = (unsigned char*)addr;
+	for(i= start - start%16; i < start; i++){
+		if((i%16) == 0){
+			printf("  %05X ", i);
+		}
+		buff[i%16] = '.';
+		printf("   ");
+	
+	}
 	// Process every byte in the data.
-	for (i = 0; i < 160; i++) {
+	for (i = start; i <= end; i++) {
 		// Multiple of 16 means new line (with line offset).
 
 		if ((i % 16) == 0) {
@@ -432,11 +502,11 @@ void hexDumpWithStartEnd(int start, int end, void *addr)
 				printf("  %s\n", buff);
 
 			// Output the offset.
-			printf("  %04X ", i);
+			printf("  %05X ", i);
 		}
 
 		// Now the hex code for the specific character.
-		printf(" %02X", pc + sizeof(char));
+		printf(" %02X", pc[i]);
 
 		// And store a printable ASCII character for later.
 		if ((pc[i] < 0x20) || (pc[i] > 0x7e)) {
@@ -448,20 +518,26 @@ void hexDumpWithStartEnd(int start, int end, void *addr)
 
 		buff[(i % 16) + 1] = '\0';
 	}
-
-	// Pad out last line if not exactly 16 characters.
-	while ((i % 16) != 0) {
+	for(i = end%16 +1; i < 16; i++){
+		buff[i%16] = '.';
 		printf("   ");
-		i++;
-	}
+	}	
 
 	// And print the final ASCII bit.
 	printf("  %s\n", buff);
 }
 
-void edit(int traget_address, int data, void *addr) {
-	unsigned char *pc = (unsigned char*)addr;
-	pc[traget_address] = data;
+void edit(int target_address, int data, void *addr) {
+	//unsigned char *pc = (unsigned char*)addr;
+	((unsigned char*)addr)[target_address] = data;
+	printf("%d %d", target_address, data);
+}
+
+void fill(int start, int end, int data, void *addr){
+	unsigned char *pc = (unsigned char*)addr;	
+	for (int i = start; i <= end; i++) {
+		pc[i] = data;
+	}
 }
 
 void insert(struct Inst inst_record, struct Record *hash_table[])
@@ -471,7 +547,6 @@ void insert(struct Inst inst_record, struct Record *hash_table[])
 	key = inst_record.opcode;
 	if (search_element(key, hash_table) != -1)
 	{
-		printf("Duplicate Key\n");
 		return;
 	}
 	h = hash_function(key);
@@ -479,26 +554,6 @@ void insert(struct Inst inst_record, struct Record *hash_table[])
 	temp->data = inst_record;
 	temp->link = hash_table[h];
 	hash_table[h] = temp;
-}
-
-void show(struct Record *hash_table[])
-{
-	int count;
-	struct Record *ptr;
-	for (count = 0; count < HASH_TABLE_MAX; count++)
-	{
-		printf("\n[%3d]", count);
-		if (hash_table[count] != NULL)
-		{
-			ptr = hash_table[count];
-			while (ptr != NULL)
-			{
-				printf("%d %s %d\t", ptr->data.opcode, ptr->data.mnemonic);
-				ptr = ptr->link;
-			}
-		}
-	}
-	printf("\n");
 }
 
 int search_element(int key, struct Record *hash_table[])
@@ -551,7 +606,7 @@ void remove_record(int key, struct Record *hash_table[])
 }
 
 void opcodelist(struct Record *hash_table[]) {
-	for (int count = 0; count < 20; count++) {
+	for (int count = 0; count < HASH_TABLE_MAX; count++) {
 		printf("%d : ", count);
 		struct Record *ptr = hash_table[count];
 		int start_flag = 1;
